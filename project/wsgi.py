8902511d32e2bed84584c33eaa62fb30ced5aa8e
aldencolerain <aldencolerain@gmail.com>
2016-02-24 00:41:10 -0500

    Added line to load environment variables in wsgi

--- project/wsgi.py
+++ project/wsgi.py
@@ -1,5 +1,8 @@
 import os
 
+try: import secrets
+except ImportError: pass
+
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 application = get_wsgi_application()
