b4930debc6cfbaa3fa6feb21c2df509ad92065cf
aldencolerain <aldencolerain@gmail.com>
2016-03-05 17:51:56 -0800

    Added view logs route

--- project/urls.py
+++ project/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^$', 'project.views.home', name='home'),
+	url(r'^logs$', 'project.views.logs', name='logs'),
 	url(r'^admin/', include(admin.site.urls)),
 ]
--- project/views.py
+++ project/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from models import *
 
 def home(request):
 	context = {}
 	return render(request, 'index.html', context)
+
+def logs(request):
+	context = {}
+	return render(request, 'logs.html', context)
